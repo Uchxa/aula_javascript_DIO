2ed08f8f93b48fdcb0802cc384dfe5ce017c90bd
Lucas Uchoa <lucas.uchxa@gmail.com>
2020-04-25 16:55:15 -0300

    primeira aula

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome =  "Lucas Uchôa"
+var idade = 18;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+//alert(`${nome} tem ${idade} anos...`);
+console.log(nome);
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+console.log(idade * idade2);
